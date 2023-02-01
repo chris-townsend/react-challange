71cd2bde2dbb39fad74e96f8ad1b697c11afb1f3
chris-townsend <chris-townsend@live.co.uk>
2023-02-01 10:43:55 +0000

    Add Controlled form technique

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+import ControlledForm from './components/ControlledForm';
 
 function App() {
   return (
     <div className='App'>
-      { <StatefulGreetingWithPrevState />}
+        <ControlledForm /> 
     </div>
   );
 }
